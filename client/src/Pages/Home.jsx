be498b0db83a539913939d296837b86c5dcd79ba
Shin <sirapop1998@gmail.com>
2025-08-21 10:14:12 +0700

    chore: update dependencies and add Tailwind CSS integration
    - Upgraded package-lock.json to lockfileVersion 3.
    - Added Tailwind CSS and Axios as dependencies.
    - Updated Vite and React Router DOM versions.
    - Configured Vite to use Tailwind CSS.
    - Created a basic Home component with Tailwind styling.
    - Added global CSS styles for body and root elements.

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+function Home() {
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">Home</h1>
+        </div>
+    )
+}
+
+export default Home;
