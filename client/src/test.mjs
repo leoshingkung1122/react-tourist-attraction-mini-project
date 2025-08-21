89cefab2e555ff135fb4e7a8e7def01455551d26
Shin <sirapop1998@gmail.com>
2025-08-21 10:39:03 +0700

    test: server response test

---
+++ client/src/test.mjs
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+async function getData() {
+    const response = await axios.get("http://localhost:4001/trips?keywords=");
+    console.log(response.data.data.length);
+}
+
+getData();
+
